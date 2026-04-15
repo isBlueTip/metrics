f679112dc2a2419c1efbc02e81e75b4c71edcd65
Simon Egorov <rhinorofl@gmail.com>
2026-04-15 18:21:48 +0700

    Add logging

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,25 +2,33 @@ package main
 
 import (
 	"flag"
-	"log"
 	"net"
 	"net/http"
 
 	"github.com/caarlos0/env/v6"
+	"github.com/isBlueTip/metrics/internal/logger"
 	"github.com/isBlueTip/metrics/internal/repository"
 	"github.com/isBlueTip/metrics/internal/server"
+	"go.uber.org/zap"
 )
 
 func run(address *ServerAddress) error {
+	if err := logger.Initialize("info"); err != nil {
+		panic(err)
+	}
+
 	mux := server.Router(repository.NewStorage())
 
 	addrString := net.JoinHostPort(address.Host, address.Port)
+
+	logger.Log.Info("Running server", zap.String("address", address.String()))
+
 	return http.ListenAndServe(addrString, mux)
 
 }
 
 func main() {
-	log.SetFlags(log.Llongfile)
+	//log.SetFlags(log.Llongfile)
 
 	addr := ServerAddress{Host: "", Port: "8080"}
 	flag.Var(&addr, "a", "Parsing net address")
--- internal/server/router.go
+++ internal/server/router.go
@@ -2,18 +2,22 @@ package server
 
 import (
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	//"github.com/go-chi/chi/v5/middleware"
 	"github.com/isBlueTip/metrics/internal/handlers"
+	//"github.com/isBlueTip/metrics/internal/logger"
+	"github.com/isBlueTip/metrics/internal/logger"
 	"github.com/isBlueTip/metrics/internal/repository"
+	//"go.uber.org/zap"
 )
 
 func Router(storage repository.Storage) *chi.Mux {
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
+	//r.Use(middleware.Logger)
+	//r.Use(logger.Log)
 	r.Route("/", func(r chi.Router) {
 		r.Get("/", handlers.GetAllMetrics(storage))
-		r.Post("/update/{metricType}/{metricName:[a-zA-Z0-9_]+}/{metricVal}", handlers.UpdateMetric(storage))
-		r.Get("/value/{metricType}/{metricName:[a-zA-Z0-9_]+}", handlers.GetMetricByName(storage))
+		r.Post("/update/{metricType}/{metricName:[a-zA-Z0-9_]+}/{metricVal}", logger.RequestLogger(handlers.UpdateMetric(storage)))
+		r.Get("/value/{metricType}/{metricName:[a-zA-Z0-9_]+}", logger.RequestLogger(handlers.GetMetricByName(storage)))
 	})
 
 	return r
